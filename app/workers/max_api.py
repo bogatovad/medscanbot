068bd50b6f2beef53e929927ea84899de2480328
Rustam <rustam.95.ur@gmail.com>
2026-02-04 22:05:24 +0500

    updated poll_max_api_status func

--- app/workers/max_api.py
+++ app/workers/max_api.py
@@ -1,4 +1,8 @@
+import base64
+import os
+import subprocess
 import uuid
+
 from pathlib import Path
 from typing import Optional
 
@@ -9,6 +13,71 @@ from app.tasks import app
 
 FINAL_TRANSACTION_STATUSES = {"cancelled", "error", "finished"}
 TARGET_STEP_TYPE = "med_permission"
+
+
+class OpenSSLSignError(Exception):
+    pass
+
+
+def create_signature_with_openssl(
+    zip_path: str,
+    transaction_id: str,
+) -> str:
+    """
+    Создаёт CMS-подпись для ZIP-файла через openssl.
+
+    zip_path       — путь к document.zip
+    transaction_id — id транзакции
+
+    Возвращает путь к созданному .sig файлу
+    """
+
+    if not os.path.exists(zip_path):
+        raise FileNotFoundError(f"ZIP not found: {zip_path}")
+
+    base_dir = os.path.join(settings.MEDIA_ROOT, "documents", transaction_id)
+    os.makedirs(base_dir, exist_ok=True)
+
+    output_sig_path = os.path.join(base_dir, f"document_{uuid.uuid4().hex}.sig")
+
+    cmd = [
+        "openssl",
+        "cms",
+        "-sign",
+        "-binary",
+        "-in", zip_path,
+        "-out", output_sig_path,
+        "-outform", "DER",
+        "-signer", settings.OPENSSL_CERT_PATH,
+        "-inkey", settings.OPENSSL_KEY_PATH,
+        "-nosmimecap",
+        "-nocerts",
+        "-noattr",
+    ]
+
+    try:
+        subprocess.run(
+            cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+    except subprocess.CalledProcessError as e:
+        raise OpenSSLSignError(
+            f"OpenSSL sign failed: {e.stderr.decode(errors='ignore')}"
+        )
+
+    return output_sig_path
+
+#
+# def unzip_archive(zip_path: str) -> str:
+#     zip_path = Path(zip_path)
+#     extract_dir = zip_path.with_suffix("")
+#
+#     with zipfile.ZipFile(zip_path, "r") as z:
+#         z.extractall(extract_dir)
+#
+#     return str(extract_dir)
 
 
 def save_zip(
@@ -37,6 +106,30 @@ def save_zip(
     return str(file_path)
 
 
+def save_sig(*, content: bytes | str, transaction_id: str) -> str:
+    """
+    Сохраняет файл подписи (.sig) в media/max_api/{transaction_id}/
+
+    content может быть:
+    - bytes
+    - base64-строкой
+    """
+
+    base_dir = os.path.join(settings.MEDIA_ROOT, "documents", transaction_id)
+    os.makedirs(base_dir, exist_ok=True)
+
+    file_path = os.path.join(base_dir, "document.sig")
+
+    # если пришёл base64
+    if isinstance(content, str):
+        content = base64.b64decode(content)
+
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+    return file_path
+
+
 @app.task(bind=True, max_retries=30, name="app.workers.max_api.poll_max_api_status")
 def poll_max_api_status(self, transaction_id: str, chat_id: int, phone_number: str):
     client = MaxApiClient()
@@ -57,18 +150,41 @@ def poll_max_api_status(self, transaction_id: str, chat_id: int, phone_number: s
             id_sig = step_data.get("idSig")
 
             if step_status == "wip_partner" or id_zip or id_sig:
+                paths = {}
+
                 if id_zip:
-                    zip_file = client.sync_document_download(
-                        transaction_id=transaction_id, field_id=id_zip, phone_number=phone_number
+                    zip_resp = client.sync_document_download(
+                        transaction_id=transaction_id,
+                        field_id=id_zip,
+                        phone_number=phone_number,
                     )
-                    file_name = save_zip(content=zip_file["content"], transaction_id=transaction_id)
-                    print(file_name)
+                    zip_path = save_zip(
+                        content=zip_resp["content"],
+                        transaction_id=transaction_id,
+                    )
+                    paths["zip"] = zip_path
+
                 if id_sig:
-                    sig_file = client.sync_document_download(
-                        transaction_id=transaction_id, field_id=id_zip, phone_number=phone_number
+                    sig_resp = client.sync_document_download(
+                        transaction_id=transaction_id,
+                        field_id=id_sig,
+                        phone_number=phone_number,
                     )
-                    file_name = save_zip(content=sig_file["content"], transaction_id=transaction_id)
-                    print(file_name)
+                    sig_path = save_sig(
+                        content=sig_resp["content"],
+                        transaction_id=transaction_id,
+                    )
+                    paths["sig"] = sig_path
+
+                if "zip" in paths:
+                    output_path = create_signature_with_openssl(
+                        zip_path=paths["zip"],
+                        transaction_id=transaction_id
+                    )
+                    upload_res = client.upload_document(file_path=output_path, transaction_id=transaction_id)
+
+                    print(upload_res)
+
                 return True
 
         if tx_status == "active":
